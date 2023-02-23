0498279b7b252a483fe05e4c782cc88b5719695c
tjq <tylercusack@gmail.com>
2023-02-23 02:11:35 -0500

    Readme and title fix

--- src/pages/[color].tsx
+++ src/pages/[color].tsx
@@ -18,7 +18,7 @@ export default function Color({ color }: Props) {
   return (
     <>
       <Head>
-        <title>#{color}</title>
+        <title>{`#${color}`}</title>
         <meta name="description" content={`#${color}`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:image" content={`/api/og-image?color=${color}`} />
